5eedab5281f47608f0c05c9b0654b03a5adeb905
宝码 <nongyoubao@alibaba-inc.com>
2017-05-09 23:45:35 +0800

    feat: 移除对 ES6 代码的编译，以及 iframe 生成文件的流程

--- src/process-demo.js
+++ src/process-demo.js
@@ -14,17 +14,6 @@ const detective = require('detective-module');
 const cleanCSS = require('./utils/clean-css');
 const demoStyleScopo = require('./utils/demo-style-scope');
 
-const babelrc = {
-  sourceMaps: 'inline',
-  presets: ['es2015', 'react', 'stage-0'].map(m => {
-    return require.resolve(`babel-preset-${m}`);
-  }),
-  plugins: [
-    require.resolve('babel-plugin-add-module-exports'),
-    require.resolve('babel-plugin-transform-decorators-legacy')
-  ]
-};
-
 function isStyleTag(node) {
   return node && JsonML.getTagName(node) === 'style';
 }
@@ -33,13 +22,7 @@ function getCode(node) {
   return JsonML.getChildren(JsonML.getChildren(node)[0])[0];
 }
 
-let tmplCache = null;
-
 module.exports = (markdownData, config) => {
-  if (!tmplCache) {
-    const templatePath = path.join(process.cwd(), config.iframeTemplate);
-    tmplCache = fs.readFileSync(templatePath).toString();
-  }
   const meta = markdownData.meta;
   meta.id = meta.filename.replace(/\.md$/, '').replace(/\//g, '-');
 
@@ -55,14 +38,8 @@ module.exports = (markdownData, config) => {
   markdownData.content = contentChildren.slice(0, codeIndex); // 移除了 pre 的内容
   markdownData.highlightedCode = contentChildren[codeIndex].slice(0, 2);
 
-  const preview = ['pre', {lang: '__react'}];
-
   const rawCode = getCode(contentChildren[codeIndex]);
-
-  preview.push(['code', rawCode]);
-
   markdownData.rawCode = rawCode;
-  markdownData.preview = preview;
   const styleNode = contentChildren.filter(node => {
     return (
       isStyleTag(node) ||
@@ -94,50 +71,5 @@ module.exports = (markdownData, config) => {
     }
   }
 
-  if (meta.iframe) {
-    const previewCode = getCode(markdownData.preview);
-    const moduleDeps = detective(previewCode);
-    const babelTransform = babel.transform(previewCode, babelrc);
-
-    const ensure = JSON.stringify(
-      moduleDeps.map(function(dep) {
-        return dep && dep.name;
-      })
-    );
-
-    const html = template(tmplCache)({
-      title: meta.title,
-      id: meta.id,
-      style: markdownData.style,
-      script: babelTransform.code,
-      map: babelTransform.map,
-      ensure: ensure
-    });
-
-    const jsFileName = `${meta.id}${config.ext}`.toLowerCase();
-    const cssFileName = `${meta.id}.css`.toLowerCase();
-
-    if (markdownData.rawStyle) {
-      // 生成 iframe css 文件。
-      fs.writeFile(
-        path.join(process.cwd(), config.outputDir, config.fileDir, cssFileName),
-        markdownData.rawStyle
-      );
-      markdownData.cssLinkPath = path.join('/', config.fileDir, cssFileName);
-    }
-
-    const demoOutputFilePath = path.join(
-      process.cwd(),
-      config.outputDir,
-      config.fileDir,
-      jsFileName
-    );
-    writeFile(demoOutputFilePath, html, err => {
-      if (err) {
-        console.log(err);
-      }
-    });
-    markdownData.src = path.join('/', config.fileDir, jsFileName);
-  }
   return markdownData;
 };
